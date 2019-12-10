0e96ba93889c1bfd62f51041cedd546279126661
Victory Moskalenko <vita.moskalenko@gmail.com>
2019-12-10 14:40:29 +0200

    Forms added

--- react-quiz/src/containers/Auth/Auth.js
+++ react-quiz/src/containers/Auth/Auth.js
@@ -1,14 +1,47 @@
 import React, { Component } from 'react';
+import classes from './Auth.module.css';
+import Button from '../../components/UI/Button/Button';
+import Input from '../../components/UI/Input/Input';
 
-class Auth extends Component {
+export default class Auth extends Component {
+
+    loginHandler = () => {
+
+    }
+
+    registerHangler = () => {
+
+    }
+
+    submitHandler = event => {
+       event.preventDefault() 
+    }
     
     render() { 
         return ( 
-            <div>
-                <h1>Auth</h1>
+            <div className={classes.Auth}>
+                <div>
+                    <h1>Authorization</h1>
+                    <form onSubmit="this.submitHandler" className={classes.AuthForm}>
+                        <Input
+                            label="Email" />
+
+                        <Input
+                            label="Password"
+                            errorMessage={'TEST'} />
+
+                        <Button
+                            type="success"
+                            onClick={this.loginHangler}>Login
+                        </Button>
+                         <Button
+                            type="primary"
+                            onClick={this.registerHangler}>Sign up
+                        </Button>
+                    </form>
+                </div>   
             </div>
          );
     }
 }
  
-export default Auth;
